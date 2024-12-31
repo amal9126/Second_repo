709bf2a35079f600f8faaa0bce30b3328039b6cc
AMAL RAJ <AMAL RAJ@LAPTOP-92ICR69A>
2025-01-01 01:47:34 +0530

    third_commit

---
+++ test_project/src/main/java/test_project/Static.java
@@ -0,0 +1,19 @@
+package test_project;
+
+
+
+public class Static {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Static.addition();
+			}
+		public static void addition()
+		{
+			int a=45,b=6,sum;
+			sum=a+b;
+			System.out.println(sum);
+		}
+	}
+
+
